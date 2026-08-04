4cdabdea64b7b2e7988276f9f5dca39f1d7992c3
Tanpaisen <tinh7demtb@gmail.com>
2026-08-04 13:45:58 +0700

    crs-frontend

---
+++ crs-frontend/src/test/java/vn/edu/crs/crsfrontend/CrsFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.crs.crsfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrsFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
